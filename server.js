204f4e32dfbcc32ab57344487c8209b7da37f2e0
sp960 <sanket.prajapati0906@gmail.com>
2024-01-04 17:04:07 +0530

    Handle Cors error in user controller and serverfile

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+const cors = require('cors')
 const app = require('./app')
 
-const port = 3000
 
+app.use(cors());
+
+app.options('*', cors());
+
+// ... Other middleware and route configurations
+
+
+const port = 3000
 app.listen(port,()=>{
             console.log(`Port is running on http://localhost:${port}`);
 })
